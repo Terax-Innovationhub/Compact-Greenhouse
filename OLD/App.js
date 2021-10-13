cc3a51e9e6f54c066d60ca1c9187b3fa6e1fadd8
Jessehoed <j.hoed@xs4all.nl>
2021-10-13 14:45:07 +0200

    Moved everything to old folder for new CLI react(better)

--- OLD/App.js
+++ OLD/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hoi Ferry!</Text> <StatusBar style="auto" />
+      <Text> Hoi Ferry</Text> <StatusBar style="auto" />
     </View>
   );
 }
