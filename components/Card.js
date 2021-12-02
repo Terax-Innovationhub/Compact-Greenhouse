f961bc8dee37ce368008d8ef5f364114a86985e4
Jessehoed <j.hoed@xs4all.nl>
2021-12-02 10:56:19 +0100

    Updated to coding standards

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {SimpleCard} from '@paraboly/react-native-card';
 
+/**
+ *
+ * @returns a card for every sensor there is
+ */
 export default function SensorCards() {
   let allCards = [
     {name: 'Sensor 1', type: 'Airsensor'},
@@ -23,46 +20,27 @@ export default function SensorCards() {
   ));
 }
 
+/**
+ *
+ * @param {*} props
+ * @returns a card
+ */
 function Card(props) {
   return (
     <SimpleCard
       title="Custom width & dynamic height simple card: Multiple Lines"
-      styles={{width: 200}}
-      cardTextStyle={{fontSize: 18}}
+      styles={styles.cardStyle}
+      cardTextStyle={styles.cardTextStyle}
       text={'Name: ' + props.name + ', Type: ' + props.type}
-      // text="Aliquip officia est cillum ipsum cupidatat occaecat ut cillum quis mollit. Aliquip ipsum officia dolor amet sint et id ut. Mollit ea ut occaecat consectetur ut laborum nisi dolor mollit culpa Lorem mollit do. Incididunt nostrud exercitation veniam exercitation ex et dolor esse laboris elit fugiat velit elit cillum."
     />
   );
 }
 
-function addCard(props) {
-  props.push('Sensor');
-}
-
-{
-  /* <SimpleCard
-  title="Custom width & dynamic height simple card: Multiple Lines"
-  styles={{width: 200, color: 'green'}}
-  cardTextStyle={{fontSize: 20}}
-  text="Aliquip officia est cillum ipsum cupidatat occaecat ut cillum quis mollit. Aliquip ipsum officia dolor amet sint et id ut. Mollit ea ut occaecat consectetur ut laborum nisi dolor mollit culpa Lorem mollit do. Incididunt nostrud exercitation veniam exercitation ex et dolor esse laboris elit fugiat velit elit cillum."></SimpleCard>; */
-}
-
-// const styles = StyleSheet.create({
-//   cardView_InsideText: {
-//     fontSize: 20,
-//     color: '#000',
-//     textAlign: 'center',
-//     marginTop: 50,
-//   },
-//   cardViewStyle: {
-//     width: 150,
-//     height: 150,
-//     backgroundColor: 'green',
-//   },
-//   MainContainer: {
-//     flex: 1,
-//     backgroundColor: 'crimson',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  cardStyle: {
+    width: 200,
+  },
+  cardTextStyle: {
+    fontSize: 18,
+  },
+});
