20ae5a25eb4be60fee5cad08695993bae5fe39bb
Jessehoed <j.hoed@xs4all.nl>
2021-12-14 13:10:06 +0100

    Removed comments

--- components/screens/Dashboard.js
+++ components/screens/Dashboard.js
@@ -1,38 +1,17 @@
 import * as React from 'react';
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import SensorCards from '../Card';
 
 export default function MyDashboard() {
   return (
-    <View
-      style={
-        styles.screens
-        // backgroundColor: 'crimson',
-      }>
+    <View style={styles.screens}>
       <SensorCards />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  card: {
-    // backgroundColor: '#f18484',
-    // justifyContent: 'center', //Centered vertically
-    // alignItems: 'center', //Centered horizontally
-  },
-  container: {
-    flex: 1,
-    // margin: 16, // Centered horizontally
-  },
   screens: {
-    backgroundColor: 'red',
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  sectionTitle: {
-    // fontSize: 24,
-    // fontWeight: '600',
-    // color: 'white',
   },
 });
